6bcb024b5a39ec8900b3407a8265df7c9d4680a6
AyushColas <144788019+AyushColas@users.noreply.github.com>
2025-03-17 10:31:00 +0530

    Time: 2280 ms (29.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+WITH RankedSalaries AS (
+    SELECT e.id, e.name AS Employee, e.salary, d.name AS Department,
+           DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT Department, Employee, salary 
+FROM RankedSalaries 
+WHERE rnk <= 3;
